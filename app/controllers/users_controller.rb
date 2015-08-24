a1a1fa705e3236ac5a3ee6189a802f12af9845f1
michiya0009 <javatea0009@gmail.com>
2015-08-24 08:48:21 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user , only:[:show, :edit, :update, :destroy ]
+  
   
   def show
     @user = User.find(params[:id])
@@ -8,20 +10,42 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  
+  
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcom to the Sample App!"
       redirect_to @user
     
-  else
-    render 'new'
+     else
+       render 'new'
     end
   end
+    
+     def edit
+     end
+  
+    
+    def update
+      if @user.update(user_params)
+        redirect_to @user, notice: '内容を更新しました。'
+      else 
+        render 'edit' 
+      end
+    end
+  
+    
 
   private
   
+   def set_user
+    @user = User.find(params[:id])
+   end
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password, :password_confimation)
-  end    
+  end 
 end
+  
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'sessions/new'
 
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
