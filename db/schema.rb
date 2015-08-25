98cf8557427cd9a6e457bff80ded9a1b3f201df9
michiya0009 <javatea0009@gmail.com>
2015-08-25 06:03:55 +0000

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824112313) do
+ActiveRecord::Schema.define(version: 20150825060316) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "body"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
